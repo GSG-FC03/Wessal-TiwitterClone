94d733d0309ead48dd2d6dcdb6e45e6f7c52f501
WessalJawad95 <wessaljawad95@gmail.com>
2021-08-21 09:29:38 +0300

    create elements (section,p,i)

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 // declare varibels to access element using dom
-let textArea=document.getElementById('textArea')
-let tweetButton=document.getElementById('tweetButton')
-let post=document.getElementById('post')
+let textArea = document.getElementById("textArea");
+let tweetButton = document.getElementById("tweetButton");
+let postContainer = document.getElementById("postContainer");
+let section, para,likePost;
 
-tweetButton.click=addPost();
-// define function to add post
-function addPost(event){
-    if(textArea.value==""||textArea.value==null)
-    {
-        alert("please add your post ^_^");
-    }
-   else{
-       event.preventDefult();
-   }
+tweetButton.onclick = addPost;
+
+// declare array to store the input value on it
+let itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+localStorage.setItem("items", JSON.stringify(itemsArray));
+
+//define add function to add input value on local storage and appear on page
+function addPost(event) {
+  if (textArea.value == "" || textArea.value == null) {
+    alert("Plaese Enter your post ^_^");
+  } else {
+    event.preventDefault();
+
+    section = document.createElement("section");
+    postContainer.appendChild(section);
+    section.setAttribute("class", "post");
+
+    para = document.createElement("p");
+    para.textContent = textArea.value;
+    section.appendChild(para);
+    para.setAttribute("calss", "paraText");
+    // para.style.width = "60%";
+    para.style.borderBottom = "1px solid red";
+
+    likePost = document.createElement("i");
+    section.appendChild(likePost);
+    likePost.setAttribute("class",'far fa-heart');
+
+    retweet = document.createElement("i");
+    section.appendChild(retweet);
+    retweet.setAttribute("class",'fas fa-retweet');
+    
+
+    itemsArray.push(textArea.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    document.querySelector("#textArea").value = "";
+  }
 }
-
-
-  
