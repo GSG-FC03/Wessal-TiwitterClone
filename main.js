2cf4a0059124016568002272d5bbb6c06dc9d366
WessalJawad95 <wessaljawad95@gmail.com>
2021-08-21 21:26:19 +0300

    edit the text when the text is longer than 250 char

--- main.js
+++ main.js
@@ -2,14 +2,12 @@
 let textArea = document.getElementById("textArea");
 let tweetButton = document.getElementById("tweetButton");
 let postContainer = document.getElementById("postContainer");
-let section, para,likePost,retweet;
+let section, para, likePost, retweet;
 
 tweetButton.onclick = addPost;
 
 // declare array to store the input value on it
-let itemsArray = localStorage.getItem("items")
-  ? JSON.parse(localStorage.getItem("items"))
-  : [];
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")): [];
 localStorage.setItem("items", JSON.stringify(itemsArray));
 
 //define add function to add input value on local storage and appear on page
@@ -19,11 +17,11 @@ function addPost(event) {
   } else {
     event.preventDefault();
   }
-    if(textArea.value.length > 250){
-        alert("the Text is longer than 250 char");
-    }
-    else{
-
+  if (textArea.value.length > 250) {
+    alert(
+      "The Text is longer than 250 charachter,please write post with maximum 250 charachter"
+    );
+  } else {
     sectionPost = document.createElement("section");
     postContainer.appendChild(sectionPost);
     sectionPost.setAttribute("class", "post");
@@ -33,22 +31,20 @@ function addPost(event) {
     sectionPost.appendChild(para);
     para.setAttribute("calss", "paraText");
     // para.style.width = "60%";
-   
+
     likePost = document.createElement("i");
     sectionPost.appendChild(likePost);
-    likePost.setAttribute("class",'far fa-heart');
-    likePost.setAttribute("id",'likeicon');
-    likePost.setAttribute('onclick','like');
+    likePost.setAttribute("class", "far fa-heart");
+    likePost.setAttribute("id", "likeicon");
+    likePost.setAttribute("onclick", "like");
 
     retweet = document.createElement("i");
     sectionPost.appendChild(retweet);
-    retweet.setAttribute("class",'fas fa-retweet');
-    retweet.setAttribute("id",'retweeticon');
-    
+    retweet.setAttribute("class", "fas fa-retweet");
+    retweet.setAttribute("id", "retweeticon");
 
     itemsArray.push(textArea.value);
     localStorage.setItem("items", JSON.stringify(itemsArray));
     document.querySelector("#textArea").value = "";
   }
 }
-
